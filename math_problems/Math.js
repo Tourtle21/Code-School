3786ea17cf9748710e7b09ae75e7dd65ffe94bd0
Tech Camp 108 <dixie108@thulium.cs.dixie.edu>
2016-06-01 16:48:00 -0600

    Added fixes to Math.game includes Jquery and added a Tic-Tac-Toe game.

--- math_problems/Math.js
+++ math_problems/Math.js
@@ -1,27 +1,48 @@
 var a = Math.floor(Math.random() * 100);
 var b = Math.floor(Math.random() * 100);
+var operator;
+var operators = {
+    '+': function(a, b) { return a + b },
+    '*': function(a, b) { return a * b },
+    '/': function(a, b) { return Math.ceil(a / b) },
+    '-': function(a, b) { return a - b },
+};
 var generateProblem = function() {
 	a = Math.floor(Math.random() * 100);
 	b = Math.floor(Math.random() * 100);
-
-	document.getElementById("a").innerHTML = a;
-	document.getElementById("b").innerHTML = b;
+	switch(Math.floor(Math.random() * 3)) {
+		case 0:
+			operator = "+"
+			break;
+		case 1:
+			operator = "*"
+			break;
+		case 2: 
+			operator = "/"
+			break;
+		default: 
+			operator = "-"
+	}
+	console.log(operator)
+	$("#operator").text(operator);
+	$("#a").text(a);
+	$("#b").text(b);
 }
 var checkAnswer = function () {
-	var input = document.getElementById("answer");
-	var answer = parseInt(input.value, 10);
+	var $input = $("#answer");
+	var answer = parseInt($input.val(), 10);
 	console.log(answer)
-	if (answer === a + b) {
+	if (answer === operators[operator](a, b)) {
 		alert("Good Job!");
 		generateProblem();
 	} else {
 		alert("incorrect!");
 	}
-	input.value = "";
+	$input.val("");
 };
 
 
 
-var button = document.getElementById("check");
-button.onclick = checkAnswer;
+var $button = $("#check")
+$button.click(checkAnswer)
 generateProblem();
